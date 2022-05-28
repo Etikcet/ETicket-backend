61c497d7f7039b6d281261063451a85f60d078e2
Kaveesha Dinamidu <kaveesha.19@cse.mrt.ac.lk>
2022-05-28 13:11:26 +0530

    services added

--- src/services/logService.js
+++ src/services/logService.js
@@ -1,7 +1,6 @@
 const userRepository = require("../repositories/userRepository");
 
 const logInUser = (username, password) => {
-  console.log(username, password);
   userRepository.getUser(username);
 };
 
