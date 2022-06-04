824d891df05cd4f041d14028a821dd3768d9ce09
Kaveesha Dinamidu <kaveesha.19@cse.mrt.ac.lk>
2022-06-04 17:02:13 +0530

    route search added

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -1,9 +1,26 @@
 const express = require("express");
+
 const authenticateToken = require("../helpers/authorization");
 
 const router = express.Router();
 
 const bookingService = require("../services/bookingService");
+
+router.post("/search", async (req, res) => {
+  try {
+    const data = await bookingService.searchBookingAvailability(req.body);
+    res.status(200);
+    res.send({
+      availableRoutes: data,
+      statusCode: 200,
+    });
+  } catch (error) {
+    res.status(400);
+    res.send({
+      error: error.message,
+    });
+  }
+});
 
 router.post(
   "/add",
--- src/repositories/bookingRepository.js
+++ src/repositories/bookingRepository.js
@@ -1,5 +1,17 @@
 const pool = require("../config/database.conf");
 
+async function searchBookingAvailabality({ start, finish }) {
+  try {
+    const res = await pool.query(
+      "SELECT * FROM route WHERE start = $1 AND finish = $2",
+      [start, finish]
+    );
+    return res;
+  } catch (error) {
+    throw Error("Internal Server Error");
+  }
+}
+
 async function addBooking(booking) {
   try {
     const res = await pool.query("INSERT INTO Booking");
@@ -11,4 +23,5 @@ async function addBooking(booking) {
 
 module.exports = {
   addBooking,
+  searchBookingAvailabality,
 };
